c0c87e4683399ec58ba559f124ce2a753e535814
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2025-09-07 18:44:23 +0330

    mvp: initial basic config structure

---
+++ drscrumbot/config.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2025  Salar Nosrati-Ershad salar@roverc.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+    provides a global config instance
+"""
+from pydantic import SecretStr, Field, field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    """
+        main application configuration using pydantic-settings
+
+        based on 12factor, the production config shall be sotored in env.
+    """
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore"
+    )
+
+    bot_token: SecretStr = Field(..., description="Telegram Bot API token")
+
+    @field_validator("bot_token")
+    @classmethod
+    def validate_bot_token(cls, v: SecretStr) -> SecretStr:
+        """
+            validator for bot token
+
+            removes leading and tailing whitespaces and insures that value
+            is not empty.
+        """
+        v: SecretStr = SecretStr(v.get_secret_value().strip())
+        if len(v):
+            return v
+        raise ValueError("BOT_TOKEN cannot be empty")
+
+
+config: Config = Config()  # it's a global instance
---
+++ drscrumbot/main.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2025  Salar Nosrati-Ershad salar@roverc.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+    main module executing the application
+"""
+import uvloop
+from drscrumbot.config import config
+
+
+async def main():
+    print(f"your bot token is: {config.bot_token}")
+
+
+if __name__ == "__main__":
+    uvloop.run(main())
